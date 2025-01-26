7772f7c50375a5448db0c0c283aecfeca5b80427
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2025-01-25 23:16:22 -0600

    se mejora el codigo de los links para que se entienda mejor

--- src/apps/Punto de venta/links.js
+++ src/apps/Punto de venta/links.js
@@ -1,14 +1,24 @@
-//get the need for create the path
+//this is for create the router of the app
 const express = require('express');
 const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('../../lib/auth');
-const apps=require('../../lib/app.js');
 
-router.get('/home-app-2', (req, res) => {
+//this is for get all the apps of the ERP
+const apps=require('../../lib/app.js'); 
+
+//this is for get the name of the folder for show his views
+const path = require('path');
+const nameApp=path.basename(__dirname); //get the name of the folder
+
+
+
+
+//*-----------------------------------------------------------routes-----------------------------------------//
+router.get('/home-app2', (req, res) => {
     const myApps=apps.get_all_my_apps();
-    res.render('links/home',{myApps});
-})
 
+    res.render(`../apps/${nameApp}/views/table`,{myApps});
+})
 
 
 
